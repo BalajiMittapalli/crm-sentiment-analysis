7717903b544e57cbe5180cd13c671d6ae06214f8
BalajiMittapalli <mittapallibalaji8@gmail.com>
2024-10-11 17:48:51 +0530

    ai updated

--- src/utils/ai.ts
+++ src/utils/ai.ts
@@ -1,32 +1,3 @@
-// import { GoogleGenerativeAI } from "@google/generative-ai";
-// import env from "./env";
-// const apiKey = env.GOOGLE_API_KEY;
-// if(!apiKey)
-//     throw new Error("Google API Key is required");
-
-// const ai = new GoogleGenerativeAI(apiKey);
-
-// const model = ai.getGenerativeModel({
-//   model: "gemini-1.5-flash",
-//   generationConfig: { responseMimeType: "application/json" }
-// });
-
-// const respond = async (
-//   prompt: string
-// ): Promise<{
-//   success: boolean;
-//   response: string;
-// }> => {
-//   try {
-//     const result = await model.generateContent(prompt);
-//     return { success: true, response: result.response.text() };
-//   } catch (error) {
-//     console.log(error);
-//     return { success: false, response: "An error occurred" };
-//   }
-// };
-
-// export { respond };
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import env from "./env";
 
